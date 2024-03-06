17aea45af2036446a4d6ef2a6d74eff0761ad6bb
dimicw <104204285+dimicw@users.noreply.github.com>
2024-03-06 17:09:24 +0100

    lab2

--- AIapp/models.py
+++ AIapp/models.py
@@ -1,4 +1,48 @@
 from django.db import models
+from django.core.files.storage import default_storage
+from tensorflow.keras.prepocessing import image as tf_image
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+import numpy as np
+
+class ImageElement(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="mediaphoto", blank=True, null=True)
+
+    def __str__(self):
+        return self.title 
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.photo:
+            try:
+                file_path = self.photo.path
+                if default_storage.exists(file_path):
+                    pil_image = tf_image.load_img(file_path, target_size=(299, 299))
+                    img_array = np.expand_dims(img_array, axis=0)
+                    img_array = preprocess_input(img_array)
+
+                    model = InceptionV3(weights='imagenet')
+                    predictions = model.predict(img_array)
+                    decoded_predictions = decode_predictions(predictions, top=1)[0]
+                    best_guess = decoded_predictions[0][1]
+                    self.title = best_guess
+                    self.content = ', '.join([f"{pred[1]}:
+                                             {pred[2] * 100:.2f}%" for pred in decoded_predictions])
+                    super().save(*args, **kwargs) 
+
+            except Exception as e:
+                pass
+
+
+
+
+
+
+
+
+
 
 # Create your models here.
 
